97f4083d0bc6282d280119e741026447dd747d44
Hu Kaixiang <maxhu787@gmail.com>
2023-07-05 01:05:28 +0800

    updates (a lot)

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((elem) => observer.observe(elem))
+
+var age, daysBetweenDates;
+daysBetweenDates = function (d1, d2) {
+  var diffDays, oneDay;
+  oneDay = 24 * 60 * 60 * 1000;
+  diffDays = (d2 - Date.parse(d1)) / oneDay;
+  return diffDays;
+};
+
+age = function () {
+  document.getElementById('myAge').innerText = (daysBetweenDates('Feb 7, 2009 00:00:00', new Date()) / 365).toFixed(8);
+};
+setInterval(age, 1200);
